ece6c87a18312c53ac634ad5498799d30c506e50
Daniel Reichhart <reichhartd@MacBook-Pro-von-Daniel.local>
2020-11-03 15:16:42 +0100

    Think in React

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import { ClassState } from './examples/ClassState';
+import { Step2 } from './examples/thinkInReact/step2';
+import { Step4 } from './examples/thinkInReact/step4';
+import { Step5 } from './examples/thinkInReact/step5';
 
 function App() {
     return (
         <div className="App">
-            <ClassState />
+            {/*<ClassState />*/}
+            {/*{Step2}*/}
+            {/*{Step4}*/}
+            {Step5}
         </div>
     );
 }
