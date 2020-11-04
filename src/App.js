5f932c516515cd91dba193f7f4b33e17c2a5d336
Daniel Reichhart <reichhartd@MacBook-Pro-von-Daniel.local>
2020-11-04 19:53:29 +0100

    Adjustements

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import './App.css';
-import { State } from './examples/state/State';
 import { Step2 } from './examples/thinkInReact/step2';
 import { Step4 } from './examples/thinkInReact/step4';
 import { Step5 } from './examples/thinkInReact/step5';
@@ -10,8 +9,7 @@ import { UseEffect } from './examples/hooks/useEffect';
 function App() {
     return (
         <div className="App">
-            {/*<ClassState />*/}
-            {/*{Step2}*/}
+            {Step2}
             {/*{Step4}*/}
             {/*{Step5}*/}
             {/*<UseState />*/}
